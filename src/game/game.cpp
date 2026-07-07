683f8f9516f68c87760825b39b2e8cf276ccaf2c
Nagendra <nx2k3@duck.com>
2026-07-07 19:34:25 +0530

    init gate base class

--- src/game/game.cpp
+++ src/game/game.cpp
@@ -1,59 +1,121 @@
 #include "game.hpp"
+#include "raylib.h"
 
 #include <cstdio>
 #include <cstdlib>
 #include <cstring>
+#include <vector>
 
-Game::Game()
-{
-    target = LoadRenderTexture(screen_width, screen_height);
-    SetTextureFilter(target.texture, TEXTURE_FILTER_BILINEAR);
+Gates::Gates(float x, float y, float gate_size) {
+  posx = x;
+  posy = y;
+  size = gate_size;
+  selected = false;
+  hover = false;
+}
+Gates::~Gates() {}
+void Gates::Draw() {
+
+  float wireLen = 100;
+  if (hover) {
+    DrawRectangle(posx - 5, posy - 5, size + 10, size + 10, ORANGE);
+  }
+  DrawRectangle(posx, posy, size, size, GREEN);
+
+  float t = 30.0;
+  Vector2 v1 = {posx, posy + t};
+  Vector2 v2 = {posx - wireLen, posy + t};
+  DrawLineEx(v1, v2, 10, BLUE);
+  DrawCircleV(v1, 5, RED);
+
+  float t1 = 70.0;
+  Vector2 v12 = {posx, posy + t1};
+  Vector2 v22 = {posx - wireLen, posy + t1};
+  DrawLineEx(v12, v22, 10, BLUE);
+
+  DrawCircleV(v12, 5, RED);
+
+  float t2 = 50.0;
+  Vector2 v13 = {posx + size, posy + t2};
+  Vector2 v23 = {posx + size + wireLen, posy + t2};
+  DrawLineEx(v13, v23, 10, BLUE);
+
+  DrawCircleV(v13, 5, RED);
 }
 
-Game::~Game()
-{
-    UnloadRenderTexture(target);
+Game::Game() {
+  target = LoadRenderTexture(screen_width, screen_height);
+  SetTextureFilter(target.texture, TEXTURE_FILTER_BILINEAR);
+  Vector2 MousePosition = {-100.0f, -100.0f};
+  gates = std::vector<Gates>();
+  gates.push_back(Gates(200, 200, 100));
+  gates.push_back(Gates(500, 500, 100));
+  gates.push_back(Gates(500, 100, 100));
 }
 
-void Game::Update()
-{
-    frame_counter++;
-}
+Game::~Game() { UnloadRenderTexture(target); }
 
-void Game::Draw()
-{
-    BeginTextureMode(target);
-    ClearBackground(RAYWHITE);
-
-    DrawRectangle(70, 90, 200, 200, BLACK);
-    DrawRectangle(70 + 16, 90 + 16, 200 - 32, 200 - 32, RAYWHITE);
-    DrawText("raylib", 70 + 200 - MeasureText("raylib", 40) - 32,
-             90 + 200 - 40 - 24, 40, BLACK);
-
-    DrawText("6.x", 290, 90 - 26, 280, BLACK);
-    DrawText("GAMEJAM", 70, 90 + 210, 120, MAROON);
-
-    if ((frame_counter / 20) % 2)
-    {
-        DrawText("are you ready?", 160, 500, 50, BLACK);
+void Game::Update() {
+  MousePosition = GetMousePosition();
+  frame_counter++;
+  for (Gates &g : gates) {
+    bool hover = CheckCollisionPointRec(
+        MousePosition, Rectangle{g.posx, g.posy, g.size, g.size});
+    if (hover && IsMouseButtonPressed(MOUSE_RIGHT_BUTTON)) {
+      g.selected = !g.selected;
     }
-
-    DrawRectangleLinesEx({0, 0, (float)screen_width, (float)screen_height}, 16, BLACK);
-
-    EndTextureMode();
-
-    BeginDrawing();
-    ClearBackground(RAYWHITE);
-
-    DrawTexturePro(target.texture,
-                   {0, 0, (float)target.texture.width, (float)-target.texture.height},
-                   {0, 0, (float)target.texture.width, (float)target.texture.height},
-                   {0, 0}, 0.0f, WHITE);
-
-    EndDrawing();
+    if (g.selected) {
+      g.posx = MousePosition.x;
+      g.posy = MousePosition.y;
+    }
+    if (hover) {
+      g.hover = true;
+    } else {
+      g.hover = false;
+    }
+  }
 }
 
-bool Game::ShouldClose() const
-{
-    return WindowShouldClose();
+void Game::Draw() {
+  BeginDrawing();
+  ClearBackground(WHITE);
+  DrawCircle(4, 4, 2, RED);
+  DrawCircle(6, 4, 2, RED);
+  for (Gates g : gates) {
+    g.Draw();
+  }
+  EndDrawing();
+  // BeginTextureMode(target);
+  // ClearBackground(RAYWHITE);
+  //
+  // DrawRectangle(70, 90, 200, 200, BLACK);
+  // DrawRectangle(70 + 16, 90 + 16, 200 - 32, 200 - 32, RAYWHITE);
+  // DrawText("raylib", 70 + 200 - MeasureText("raylib", 40) - 32,
+  //          90 + 200 - 40 - 24, 40, BLACK);
+  //
+  // DrawText("6.x", 290, 90 - 26, 280, BLACK);
+  // DrawText("GAMEJAM", 70, 90 + 210, 120, MAROON);
+  //
+  // if ((frame_counter / 20) % 2)
+  // {
+  //     DrawText("are you ready?", 160, 500, 50, BLACK);
+  // }
+  //
+  // DrawRectangleLinesEx({0, 0, (float)screen_width, (float)screen_height}, 16,
+  // BLACK);
+  //
+  // EndTextureMode();
+  //
+  // BeginDrawing();
+  // ClearBackground(RAYWHITE);
+  //
+  // DrawTexturePro(target.texture,
+  //                {0, 0, (float)target.texture.width,
+  //                (float)-target.texture.height}, {0, 0,
+  //                (float)target.texture.width, (float)target.texture.height},
+  //                {0, 0}, 0.0f, WHITE);
+  //
+  // EndDrawing();
 }
+
+bool Game::ShouldClose() const { return WindowShouldClose(); }
--- src/game/game.hpp
+++ src/game/game.hpp
@@ -1,7 +1,7 @@
 #pragma once
 
 #include <raylib.h>
-
+#include <vector>
 #if defined(PLATFORM_WEB)
 #include <emscripten/emscripten.h>
 #endif
@@ -9,26 +9,32 @@
 constexpr int screen_width = 720;
 constexpr int screen_height = 720;
 
-enum class GameScreen 
-{
-    Logo,
-    Title,
-    Gameplay,
-    Ending
-};
-
-class Game 
-{
+enum class GameScreen { Logo, Title, Gameplay, Ending };
+class Gates {
 public:
-    Game();
-    ~Game();
+  Gates(float x, float y, float size);
+  ~Gates();
+  void Draw();
+  // public for now
+  float posx;
+  float posy;
+  float size;
+  bool hover;
+  bool selected;
+};
+class Game {
+public:
+  Game();
+  ~Game();
 
-    void Update();
-    void Draw();
-    bool ShouldClose() const;
+  void Update();
+  void Draw();
+  bool ShouldClose() const;
 
 private:
-    RenderTexture2D target{};
-    int frame_counter{};
-    GameScreen current_screen{GameScreen::Logo};
+  RenderTexture2D target{};
+  int frame_counter{};
+  Vector2 MousePosition;
+  GameScreen current_screen{GameScreen::Logo};
+  std::vector<Gates> gates;
 };
